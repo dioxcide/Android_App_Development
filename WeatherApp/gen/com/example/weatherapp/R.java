def859e5b39ffc78d647657ef844571b0908a85d
dioxcide <clown_krown@yahoo.com>
2014-08-16 21:21:38 -0500

    Basically Chicago Only Weather App

--- WeatherApp/gen/com/example/weatherapp/R.java
+++ WeatherApp/gen/com/example/weatherapp/R.java
@@ -25,9 +25,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_refresh=0x7f080004;
-        public static final int action_settings=0x7f080003;
+        public static final int action_refresh=0x7f080005;
+        public static final int action_settings=0x7f080004;
         public static final int container=0x7f080000;
+        public static final int exit=0x7f080003;
         public static final int list_item_forecast_textview=0x7f080002;
         public static final int listview_forecast=0x7f080001;
     }
@@ -37,13 +38,15 @@ public final class R {
         public static final int list_item_forecast=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
-        public static final int refresh=0x7f070001;
+        public static final int exit=0x7f070000;
+        public static final int main=0x7f070001;
+        public static final int refresh=0x7f070002;
     }
     public static final class string {
         public static final int action_refresh=0x7f050003;
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int exit=0x7f050004;
         public static final int hello_world=0x7f050001;
     }
     public static final class style {
